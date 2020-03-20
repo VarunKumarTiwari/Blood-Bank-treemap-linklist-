ed6ad3753e9511eb2660c236606302d8843696b2
tiwarivarun <tiwarivarun@users.noreply.github.com>
2020-03-20 14:23:23 +0000

    tree print

--- src/com/BBTL/GUI/BinarySearchTree.java
+++ src/com/BBTL/GUI/BinarySearchTree.java
@@ -1,74 +1,81 @@
 package com.BBTL.GUI;
-// Java program to demonstrate insert operation in binary search tree 
 class BinarySearchTree { 
-
-	/* Class containing left and right child of current node and key value*/
+public int level = 0;
 	class Node { 
 		int key; 
 		Node left, right; 
-
+	
 		public Node(int item) { 
-			key = item; 
+			
+			key = item;
 			left = right = null; 
+		
 		} 
 	} 
 
-	// Root of BST 
-	Node root; 
+	  static 
+	Node root;
 
-	// Constructor 
+	 
 	BinarySearchTree() { 
 		root = null; 
 	} 
 
-	// This method mainly calls insertRec() 
 	void insert(int key) { 
 	root = insertRec(root, key); 
 	} 
 	
-	/* A recursive function to insert a new key in BST */
 	Node insertRec(Node root, int key) { 
 
-		/* If the tree is empty, return a new node */
 		if (root == null) { 
 			root = new Node(key); 
 			return root; 
 		} 
 
-		/* Otherwise, recur down the tree */
 		if (key < root.key) 
+		{
 			root.left = insertRec(root.left, key); 
-		else if (key > root.key) 
+		}else if (key > root.key) 
 			root.right = insertRec(root.right, key); 
 
-		/* return the (unchanged) node pointer */
 		return root; 
 	} 
 
-	// This method mainly calls InorderRec() 
 	void inorder() { 
 	inorderRec(root); 
 	} 
+	
+	
+		void inorderRec(Node root) { 
+		if (root != null) {
 
-	// A utility function to do inorder traversal of BST 
-	void inorderRec(Node root) { 
-		if (root != null) { 
 			inorderRec(root.left); 
 			System.out.println(root.key); 
 			inorderRec(root.right); 
+			
 		} 
 	} 
+	void printTree(Node node, String prefix)
+	 {
+	    if(node == null) return;
+	    
+	    
+	    System.out.println(prefix + " + " + node.key);
+	    printTree(node.left , prefix + " ");
+	    printTree(node.right , prefix + " ");
+	    
+	    
+	 }
 
-	// Driver Program to test above functions 
 	public static void main(String[] args) { 
 		BinarySearchTree tree = new BinarySearchTree(); 
 
 		/* Let us create following BST 
-			50 
+		  50 
 		/	 \ 
-		30	 70 
-		/ \ / \ 
-	20 40 60 80 */
+	   30	 70 
+	  / \   /  \ 
+	20  40 60  80 */
 		tree.insert(50); 
 		tree.insert(30); 
 		tree.insert(20); 
@@ -77,8 +84,8 @@ class BinarySearchTree {
 		tree.insert(60); 
 		tree.insert(80); 
 
-		// print inorder traversal of the BST 
-		tree.inorder(); 
+		//tree.inorder(); 
+		tree.printTree(root, "");
+		
 	} 
 } 
-// This code is contributed by Ankur Narain Verma 
