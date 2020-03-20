953953112350fc967eaaf9e478d61f05263388a7
tiwarivarun <tiwarivarun@users.noreply.github.com>
2020-03-20 19:46:44 +0000

    random ID

--- src/com/BBTL/GUI/BinarySearchTree.java
+++ src/com/BBTL/GUI/BinarySearchTree.java
@@ -1,91 +1,115 @@
 package com.BBTL.GUI;
-class BinarySearchTree { 
-public int level = 0;
-	class Node { 
-		int key; 
-		Node left, right; 
-	
-		public Node(int item) { 
-			
+
+class BinarySearchTree {
+	public int level = 0;
+
+	class Node {
+		int key;
+		Node left, right;
+
+		public Node(int item) {
+
 			key = item;
-			left = right = null; 
-		
-		} 
-	} 
+			left = right = null;
 
-	  static 
-	Node root;
+		}
+	}
 
-	 
-	BinarySearchTree() { 
-		root = null; 
-	} 
+	static Node root;
 
-	void insert(int key) { 
-	root = insertRec(root, key); 
-	} 
-	
-	Node insertRec(Node root, int key) { 
+	BinarySearchTree() {
+		root = null;
+	}
 
-		if (root == null) { 
-			root = new Node(key); 
-			return root; 
-		} 
+	void insert(int key) {
+		root = insertRec(root, key);
+	}
 
-		if (key < root.key) 
-		{
-			root.left = insertRec(root.left, key); 
-		}else if (key > root.key) 
-			root.right = insertRec(root.right, key); 
+	Node insertRec(Node root, int key) {
 
-		return root; 
-	} 
+		if (root == null) {
+			root = new Node(key);
+			return root;
+		}
 
-	void inorder() { 
-	inorderRec(root); 
-	} 
-	
-	
-		void inorderRec(Node root) { 
+		if (key < root.key) {
+			root.left = insertRec(root.left, key);
+		} else if (key > root.key)
+			root.right = insertRec(root.right, key);
+
+		return root;
+	}
+
+	void inorder() {
+		inorderRec(root);
+	}
+
+	void inorderRec(Node root) {
 		if (root != null) {
 
-			inorderRec(root.left); 
-			System.out.println(root.key); 
-			inorderRec(root.right); 
-			
-		} 
+			inorderRec(root.left);
+			System.out.println(root.key);
+			inorderRec(root.right);
+
+		}
+	}
+	public static void printBinaryTree(Node root, int level){
+	    if(root==null)
+	         return;
+	    printBinaryTree(root.right, level+1);
+	    if(level!=0){
+	        for(int i=0;i<level-1;i++)
+	            System.out.print("|\t");
+	            System.out.println("|-------"+root.key);
+	    }
+	    else
+	        System.out.println(root.key);
+	    printBinaryTree(root.left, level+1);
 	} 
-	void printTree(Node node, String prefix)
-	 {
-	    if(node == null) return;
-	    
-	    
-	    System.out.println(prefix + " + " + node.key);
-	    printTree(node.left , prefix + " ");
-	    printTree(node.right , prefix + " ");
-	    
-	    
-	 }
 
-	public static void main(String[] args) { 
-		BinarySearchTree tree = new BinarySearchTree(); 
-
-		/* Let us create following BST 
-		  50 
-		/	 \ 
-	   30	 70 
-	  / \   /  \ 
-	20  40 60  80 */
-		tree.insert(50); 
-		tree.insert(30); 
-		tree.insert(20); 
-		tree.insert(40); 
-		tree.insert(70); 
-		tree.insert(60); 
-		tree.insert(80); 
-
-		//tree.inorder(); 
-		tree.printTree(root, "");
+	public static void main(String[] args) {
+		boolean flag = true;
+		BinarySearchTree tree = new BinarySearchTree();
+		int arr[] = new int[30] ;
+		int id = (int) (Math.random() * (1 - 30) + 30);
+		for(int i=0;i<arr.length;i++)
+		{
+			 if(arr[i]==id)
+			 {
+				 flag = false;
+			 }
+		}
+//		if()
+//		{
+//			
+//		}
 		
-	} 
-} 
+		//Collections.shuffle(Arrays.asList(arr));
+		System.out.println(arr);
+		//Collections.shuffle(Arrays.asList(arr));
+		/*
+		 * Let us create following BST 
+		 * 50 
+		 * / \ 
+		* 30 70 
+	 *   / \ / \ 
+	 * 20 40 60 80
+		 */
+//		tree.insert(50);
+//		tree.insert(30);
+//		tree.insert(20);
+//		tree.insert(40);
+//		tree.insert(70);
+//		tree.insert(60);
+//		tree.insert(80);
+//		tree.insert(90);
+//		
+//		tree.insert(66);
+//		tree.insert(100);
+//		tree.insert(90);
+	   // tree.inorder();
+		
+		tree.printBinaryTree(root,0);
+
+	}
+}
